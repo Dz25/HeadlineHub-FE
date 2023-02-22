610f47eaf438123b4425a55e7b008447e553967e
mayumi <yamayun0x0@gmail.com>
2023-02-21 23:43:26 -0800

    First Commit

---
+++ src/components/Weather.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- This is an example for layout validation. It will be changed later. -->
+  <section class="" style="background-color: gainsboro;">
+    <h4 class="pt-2 ps-2">Weather</h4>
+    <div class="container pb-3">
+
+      <div class="row d-flex justify-content-center align-items-center">
+        <div class="">
+
+          <div class="card" style="color: #4B515D; border-radius: 30px;">
+            <div class="card-body p-4">
+
+              <div class="d-flex">
+                <h6 class="flex-grow-1">Warsaw</h6>
+                <h6>15:07</h6>
+              </div>
+
+              <div class="d-flex flex-column text-center mt-5 mb-4">
+                <h6 class="display-4 mb-0 font-weight-bold" style="color: #1C2331;"> 13°C </h6>
+                <span class="small" style="color: #868B94">Stormy</span>
+              </div>
+
+              <div class="d-flex align-items-center">
+                <div class="flex-grow-1" style="font-size: 1rem;">
+                  <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 40 km/h
+                    </span></div>
+                  <div><i class="fas fa-tint fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 84% </span>
+                  </div>
+                  <div><i class="fas fa-sun fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 0.2h </span>
+                  </div>
+                </div>
+                <div>
+                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu1.webp" width="100px">
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,58 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import Card from '../components/Card.vue'
+import Weather from '../components/Weather.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+   <main>
+      <div class="container-fluid mt-5 pt-5">
+         <div class="row">
+            <div class="col-md-8">
+               <!-- Latest News -->
+               <Card />
+               <Card />
+
+            </div>
+            <div class="col-md-4">
+               <Weather />
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-7">
+               <!-- General News -->
+            </div>
+            <div class="col-md-5">
+               <!-- Nearby News -->
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-6">
+               <!-- Business News -->
+            </div>
+            <div class="col-md-6">
+               <!-- Entertainment News -->
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-6">
+               <!-- Health News -->
+            </div>
+            <div class="col-md-6">
+               <!-- Science News -->
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-6">
+               <!-- Sports News -->
+            </div>
+            <div class="col-md-6">
+               <!-- Tech News -->
+            </div>
+         </div>
+      </div>
+
+
+      <!-- <TheWelcome /> -->
+   </main>
 </template>
