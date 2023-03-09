b446f9aed1c5f6c2a3910fe56f080f4031bf6f60
Dzung <duydung25082003@gmail.com>
2023-03-09 00:34:20 -0800

    Add summarization function

--- src/components/ArticleModal.vue
+++ src/components/ArticleModal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="modal fade" tabindex="-1" :id="'article' + id" aria-hidden="true">
+    <div class="modal modal-lg modal-dialog-scrollable fade" tabindex="-1" :id="'article' + id" aria-hidden="true" ref="modalEle">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -12,17 +12,16 @@
                     <img :src=urlToImage class="img-fluid" :alt=title>
                     <figure class="p-1 mt-1">
                         <blockquote class="blockquote">
-                            <p>{{ description }}</p>
+                            <p>{{ summary }}</p>
                         </blockquote>
                         <figcaption class="blockquote-footer text-end">
                             {{ source?.name }}
                         </figcaption>
                     </figure>
-                   
+
                 </div>
                 <div class="modal-footer">
                     <a :href=url class="btn btn-primary">More details</a>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
@@ -30,11 +29,44 @@
 </template>
 
 <script setup>
-import { toRefs } from 'vue';
+import { toRefs,ref,onMounted } from 'vue';
+import { Modal } from "bootstrap";
+// onMounted(() => {
+//     const formdata = new FormData();
+//     formdata.append("key", "7eaa2e91d0e5ab1c39662ff390ebbe4b");
+//     formdata.append("url", "https://www.wired.com/story/oneplus-pad-android-tablet/");
+//     formdata.append("sentences", "5");
+
+//     const requestOptions = {
+//         method: 'POST', 
+//         body: formdata,
+//         redirect: 'follow'
+//     };
+
+//     const response = fetch("https://api.meaningcloud.com/summarization-1.0", requestOptions)
+//         .then(response => ({
+//             status: response.status,
+//             body: response.json()
+//         }))
+//         .then(({body }) => console.log(body))
+//         .catch(error => console.log('error', error));
+// })
+
+// const summary = ref()
 
 const props = defineProps({
     data: Object,
     id: String,
+    summary: String
 })
 const { source, title, description, url, urlToImage, publishedAt } = toRefs(props.data)
+let modalEle = ref(null);
+let thisModalObj = null;
+onMounted(() => {
+  thisModalObj = new Modal(modalEle.value);
+});
+function _show() {
+  thisModalObj.show();
+}
+defineExpose({ show: _show });
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,46 +1,16 @@
-<script setup>
-import { toRefs,ref } from 'vue';
-import axios from 'axios';
-import ArticleModal from './ArticleModal.vue';
-
-  const props = defineProps({
-    article: Object
-  })
-  // const summary = ref()
-  // const handleModal = () =>{
-  //   let formData = new FormData()
-  //   formData.append("key", "7eaa2e91d0e5ab1c39662ff390ebbe4b")
-  //   formData.append("url", url)
-  //   formData.append("sentences", 5)
-  //   axios.post('https://api.meaningcloud.com/summarization-1.0', formData)
-  // .then(response => {
-  //   console.log(response.data);
-  // })
-  // .catch(error => {
-  //   console.log(error);
-  // });
-
-    
-  // }
-
-  const id = crypto.randomUUID();
-  const {author,title, description, urlToImage, publishedAt } = toRefs(props.article)
-  
-</script>
-
 <template>
-    <div class="card">
-      <img :src=urlToImage class="card-img-top" :alt=title />
-      <div class="card-body">
-        <h5 class="card-title">{{ title }}</h5>
-        <p class="card-text">{{ description }}</p>
-        <p class="card-text"><small class="text-muted">{{ new Date(Date.parse(publishedAt)) }}</small></p>
-        <p class="card-text"><small class="text-muted">By {{ author }}</small></p>
-        <button class="btn btn-primary" data-bs-toggle="modal" :data-bs-target="'#article'+id" @click="handleModal">Read More</button>
-        <ArticleModal :data=article :id = id />
-      </div>
+  <div class="card">
+    <img :src=urlToImage class="card-img-top" :alt=title />
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">{{ description }}</p>
+      <p class="card-text"><small class="text-muted">{{ new Date(Date.parse(publishedAt)) }}</small></p>
+      <p class="card-text"><small class="text-muted">By {{ author }}</small></p>
+      <button class="btn btn-primary" @click="showModal">Read More</button>
+      <ArticleModal ref="thisModal" :data=article :id=id :summary=summary />
     </div>
-  </template>
+  </div>
+</template>
   
 <!-- <template>
     <div class="card border-0 rounded-0">
@@ -52,3 +22,36 @@ import ArticleModal from './ArticleModal.vue';
         </div>
     </div>
 </template> -->
+
+<script setup>
+import { toRefs, ref } from 'vue';
+import ArticleModal from './ArticleModal.vue';
+
+const props = defineProps({
+  article: Object
+})
+const thisModal= ref(null)
+const id = crypto.randomUUID()
+const summary = ref("")
+const { author, title, description, urlToImage, publishedAt } = toRefs(props.article)
+
+const showModal = async () => {
+  const formdata = new FormData();
+  formdata.append("key", "7eaa2e91d0e5ab1c39662ff390ebbe4b");
+  formdata.append("url", "https://www.wired.com/story/oneplus-pad-android-tablet/");
+  formdata.append("sentences", "5");
+
+  const requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+  };
+
+  const respone = await fetch("https://api.meaningcloud.com/summarization-1.0", requestOptions)
+  const data = await respone.json()
+  console.log(data)
+  summary.value = data.summary
+  thisModal.value.show()
+}
+
+</script>
