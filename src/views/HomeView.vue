c56730450d2ee205d1b2d7f376e65962ecf5c250
jaydipkmulani <jaydipkmulani@gmail.com>
2023-03-10 22:22:31 -0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <script setup>
+<<<<<<< Updated upstream
 import TheWelcome from '../components/TheWelcome.vue'
+=======
+// import TheWelcome from '../components/TheWelcome.vue'
+import Card from '../components/Card.vue'
+// import Weather from '../components/Weather.vue'
+import NoImgNews from '../components/NoImgNews.vue';
+
+const article = {
+   source: { id: 'wired', name: 'Wired' },
+   author: 'Boone Ashworth',
+   title: 'OnePlus Pad Android Tablet: Details, Specs, Release Date',
+   description: 'Plus: Netflix expands its efforts to end password sharing, Apple’s “buy now, pay later” entry creeps closer, and we scrutinize your Bitcoin spending.',
+   url: 'https://www.wired.com/story/oneplus-pad-android-tablet/',
+   urlToImage: 'https://media.wired.com/photos/63e69faddcab861f7a47469f/191:100/w_1280,c_limit/OnePlus-Pad-Gear-Roundup-Featured-Gear.jpg',
+   publishedAt: '2023-02-14T12:00:00Z'
+}
+>>>>>>> Stashed changes
 </script>
 
 <template>
